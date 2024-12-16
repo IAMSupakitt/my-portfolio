51acd46bfabb47765bf72b9875d8b8847c124efc
IAMSupakitt <spk.supakitt.im@gmail.com>
2024-12-16 20:53:06 +0700

    updatefile

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-// import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./components/header";
+import Footer from "./components/header";
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <h1 className="text-5xl font-bold underline">
-      Hello test world!
-    </h1>
+    <>
+      <Header />
+      <div className="h-screen ">
+        <h1 className=" flex text-5xl font-bold underline">
+          Hello test world!
+        </h1>
+      </div>
+
+      <BrowserRouter>
+        <Routes>
+          {/*<Route path='/register' element={<Register/>}/>*/}
+        </Routes>
+      </BrowserRouter>
+
+      <Footer />
+    </>
   );
 }
 
